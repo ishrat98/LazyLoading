5566059aa50639129bfdf660056c7c2ffbf7a6c0
emu <bsse0927@iit.du.ac.bd>
2020-02-03 18:27:21 +0600

    upto profile

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PictureComponent } from './picture/picture.component';
-import { LoginComponent } from './login/login.component';
+
 
 
 
 @NgModule({
-  declarations: [PictureComponent, LoginComponent],
+  declarations: [PictureComponent],
   imports: [
     CommonModule
   ]
